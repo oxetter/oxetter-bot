de053d4b458dc84f574f6f4179966b01a9e5e3d7
oxetter <okseterov@gmail.com>
2026-09-20 22:13:12 +0600

    Fix alert message for non-subscribers

--- bot.py
+++ bot.py
@@ -2128,7 +2128,6 @@ def cb_admin_stats(call):
     if not ADMIN_ID or uid != ADMIN_ID:
         bot.answer_callback_query(call.id, "❌ Нет доступа", show_alert=True)
         return
-    bot.answer_callback_query(call.id, "❌ Не подписан!", snow_alert=True)
     bot.send_message(call.message.chat.id, "📊 Считаю статистику...")
     try:
         s = admin_stats()
